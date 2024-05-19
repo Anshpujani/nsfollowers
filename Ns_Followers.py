26e6c0d13f0921ea52f7c7d003266d11ad9081ed
Anshpujani <tysonff27@gmail.com>
2024-05-19 11:01:39 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='66625809944' #Enter UserID Here
+        self.__username='kaludoon01' #Enter UserName Here
+        self._session='66625809944%3Aqp4VGkSF3NgklA%3A25%3AAYccpSotZe5LN2VAdJ8xx1hr8vVioHUcBOuJ5xeuww' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
